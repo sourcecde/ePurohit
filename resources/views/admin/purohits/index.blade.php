6c416489a2a3eaf63e7f1291f89a8842117cca83
Debraj <dbrj.prof@gmail.com>
2020-01-07 14:18:13 +0530

    datatable view update

--- resources/views/admin/purohits/index.blade.php
+++ resources/views/admin/purohits/index.blade.php
@@ -19,7 +19,7 @@
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-Purohit">
+            <table class=" table table-bordered table-striped table-hover datatable datatable-Purohit" style="margin-left: 0px; width: 100%;">
                 <thead>
                     <tr>
                         <th width="10">
